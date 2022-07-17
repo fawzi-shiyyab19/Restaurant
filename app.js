b7a107c63993e7ce9e6e9d83d152926a48907531
Fawzi-shiyyab19 <fawziakef@icloud.com>
2022-07-17 11:31:27 +0300

    new

--- app.js
+++ app.js
@@ -1,61 +1,37 @@
-'use strict';
+"use strict";
 
-var allInformation=[];
-var parent = document.getElementById('parent');
-var table = document.createElement('table');
-parent.appendChild(table);
+const formEl = document.getElementById("form");
+const perentEl = document.getElementById("table");
 
-function foodInformation(food_id,foodname,type,price){
-    this.food_id = food_id;
-    this.foodname=foodname;
-    this.type=type;
-    this.price=price;
-    allInformation.push(this);
+let countID = 1;
+function Foodinformation(name, type, price) {
+  this.id = countID++;
+  this.name = name;
+  this.type = type;
+  this.price = price;
 }
 
+formEl.addEventListener("submit", handelSubmit);
 
-var tableHeader    = ["food_id","foodname","price","type"]
+function handelSubmit(event) {
+  event.preventDefault();
 
-
-foodInformation.prototype.Tables = function(){
-if (allInformation.length > 0){
-for (let j = 0 ; j < tableHeader.length; j++){var tr=document.createElement("tr");
-table.appendChild(tr);
-    var cell1 = document.createElement('td');
-    tr.appendChild(cell1);
-    cell1.textContent=allInformation[j].food_id;
-
-    var cell2 = document.createElement('td');
-    tr.appendChild(cell2);
-    cell2.textContent=allInformation[j].foodname;
-    var cell3 = document.createElement('td');
-    tr.appendChild(cell3);
-    cell3.textContent=allInformation[j].price;
-    var cell4 = document.createElement('td');
-    tr.appendChild(cell4);
-    cell4.textContent=allInformation[j].type;
-
-}}
-
+  let name = event.target.foodName.value;
+  let type = event.target.foodType.value;
+  let price = event.target.foodPrice.value;
+  let food = new Foodinformation(name, type, price);
+  food.render();
 }
 
-var obj1=new foodInformation("1","pizza","fat","2.5")
-var obj2=new foodInformation("2","kabseh","protein","2.5")
-var obj3=new foodInformation("3","mansaf","fat","5.5")
+Foodinformation.prototype.render = function () {
+  let trEl = document.createElement("tr");
 
-for (var i = 0 ; i < allInformation.length ; i++){
-    allInformation[i].Tables();
-}
-console.log(allInformation);
+  trEl.innerHTML = `
+    <td>${this.id}</td>
+    <td>${this.name}</td>
+    <td>${this.type}</td>
+    <td>$${this.price}</td>
+  `;
 
-var form = document.getElementById('form');
-form.addEventListener('submit', function (event){
-event.preventDefault();
-console.log(event);
-var food_id = event.target.food_id.value;
-var foodname = event.target.foodname.value;
-var type = event.target.type.value;
-var price = event.target.price.value;
-const objForm = new foodInformation(food_id, foodname, type, price);
-objForm.Table();
-});
+  perentEl.appendChild(trEl);
+};
