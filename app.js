bcad3c4263214c2cd967e7fb54b05c371b95e9e8
Fawzi-shiyyab19 <fawziakef@icloud.com>
2022-07-17 20:33:09 +0300

    let local branch

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 const formEl = document.getElementById("form");
 const perentEl = document.getElementById("table");
 
 let countID = 1500;
-function Foodinformation(name, type, price) {
+function Food(name, type, price) {
   this.id = countID++;
   this.name = name;
   this.type = type;
@@ -19,11 +19,11 @@ function handelSubmit(event) {
   let name = event.target.foodName.value;
   let type = event.target.foodType.value;
   let price = event.target.foodPrice.value;
-  let food = new Foodinformation(name, type, price);
+  let food = new Food(name, type, price);
   food.render();
 }
 
-Foodinformation.prototype.render = function () {
+Food.prototype.render = function () {
   let trEl = document.createElement("tr");
 
   trEl.innerHTML = `
---
+++ statistics.js
@@ -0,0 +1,49 @@
+'use strict';
+let countiD = 1000;
+let foodList = [];
+const perentEl = document.getElementById("table");
+
+function Food(name, type, price) {
+  this.id = countID++;
+  this.name = name;
+  this.type = type;
+  this.price = price;
+
+  foodList.push(this);
+}
+
+Food.prototype.render = function () {
+  let trEl = document.createElement("tr");
+  let tdId = document.createElement("td");
+  let tdName = document.createElement("td");
+  let tdType = document.createElement("td");
+  let tdPrice = document.createElement("td");
+
+  tdId.textContent = this.id;
+  tdName.textContent = this.name;
+  tdType.textContent = this.type;
+  tdPrice.textContent = `$ ${this.price}`;
+
+  trEl.appendChild(tdId);
+  trEl.appendChild(tdName);
+  trEl.appendChild(tdType);
+  trEl.appendChild(tdPrice);
+
+  perentEl.appendChild(trEl);
+};
+
+function getData() {
+  let parsedData = JSON.parse(localStorage.getItem("Foods"));
+
+  if (parsedData) {
+    for (let i = 0; i < parsedData.length; i++) {
+      new Food(parsedData[i].name, parsedData[i].type, parsedData[i].price);
+    }
+  }
+
+  
+  for (let i = 0; i < foodList.length; i++) {
+    foodList[i].render();
+  }
+}
+getData();
